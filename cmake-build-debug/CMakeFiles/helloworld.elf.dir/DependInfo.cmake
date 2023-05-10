2d76820044dc6f53aa9303fc32122835e3d8932d
Yennie <pyx2816459347@gmail.com>
2023-05-10 19:49:38 +0800

    first_try

---
+++ cmake-build-debug/CMakeFiles/helloworld.elf.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Startup/startup_stm32f407ighx.s" "D:/Users/Yennie/pyx/STM32/helloworld/cmake-build-debug/CMakeFiles/helloworld.elf.dir/Core/Startup/startup_stm32f407ighx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Inc"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/gpio.c" "CMakeFiles/helloworld.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/main.c" "CMakeFiles/helloworld.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/helloworld.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/stm32f4xx_it.c" "CMakeFiles/helloworld.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/syscalls.c" "CMakeFiles/helloworld.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/sysmem.c" "CMakeFiles/helloworld.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Core/Src/system_stm32f4xx.c" "CMakeFiles/helloworld.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/Users/Yennie/pyx/STM32/helloworld/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/helloworld.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj: \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_pwr.c \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Core\Inc/stm32f4xx_hal_conf.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Include/core_cm4.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ d:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ D:\Users\Yennie\pyx\STM32\helloworld\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h
